b65400693c16b8be037a4d38a088e46dfe0e7a54
sarait2 <saraitorres999@gmail.com>
2024-11-24 22:54:04 -0500

    Setup root component with global state and routing.

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Gallery from './Gallery';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tour Comparison App</h1>
+      <Gallery />
+    </div>
+  );
+};
+
+export default App;
